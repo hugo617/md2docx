80fe64e4c8cdf68c6e879d73f26e66eb6fda38c4
star <star@starworking.com>
2026-04-09 23:32:43 +0800

    feat: add default style configuration (style_map.py)

---
+++ src/core/style_map.py
@@ -0,0 +1,22 @@
+DEFAULT_STYLE_CONFIG = {
+    "fonts": {
+        "heading": "SimHei",
+        "heading_east_asia": "黑体",
+        "body": "SimSun",
+        "body_east_asia": "宋体",
+        "code": "Consolas",
+    },
+    "sizes": {
+        "h1": 22,
+        "h2": 16,
+        "h3": 14,
+        "body": 12,
+        "code": 9,
+    },
+    "colors": {
+        "code_bg": "F5F5F5",
+        "table_header_bg": "E8E8E8",
+        "quote_border": "CCCCCC",
+        "link": "0000EE",
+    },
+}
